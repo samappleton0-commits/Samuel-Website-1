e18fbff73be881988f54810e6b03726d11a675ec
Samuel R. Appleton <samappleton0@gmail.com>
2026-07-21 00:56:05 -1200

    Added Vercel Speed Insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { Toaster } from 'sonner'
 import './globals.css'
 import { SiteHeader } from '@/components/site-header'
 import { SiteFooter } from '@/components/site-footer'
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import WelcomeToast from '@/components/WelcomeToast'
 
 
